40adec541588282d24cb7331d9f9d58ed0d23a0d
Katherin Jimenez <katjimenez98@gmail.com>
2026-04-27 13:49:38 -0400

    git

--- trackermax-api/src/app.ts
+++ trackermax-api/src/app.ts
@@ -13,7 +13,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://katherinj.github.io"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/projects", projectsRouter);
